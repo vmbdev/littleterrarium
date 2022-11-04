dc9d799848a2b5554f7d1b2e336693e8d3d57dc0
Borja <borjavidesm@gmail.com>
2022-11-04 02:12:16 +0100

    Fix to incorrect import

--- src/app/plant/plant-edit-watering/plant-edit-watering.component.ts
+++ src/app/plant/plant-edit-watering/plant-edit-watering.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import * as dayjs from 'dayjs';
-import { Plant } from '@prisma/client';
+import { Plant } from 'src/app/interfaces';
 import { PlantService } from '../plant.service';
 
 @Component({
